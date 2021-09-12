e7d43676de549300993602e2d37fca1f9de51eb1
Jack King <jkingofficial01@gmail.com>
2021-09-12 17:36:23 +1000

    fourth commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,31 @@
-console.log('this is not working')
+console.log('JS Is Working!')
+
+//* Injecting Header w/ fetch
+
+fetch("../html/header.html")
+    .then(response =>{
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+//* Injecting Footer w/ fetch
+
+fetch("../html/footer.html")
+    .then(response =>{
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
+
+//* Injecting Navbar w/ fetch
+
+fetch("../html/navbar.html")
+    .then(response =>{
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("nav").innerHTML = data;
+    });
